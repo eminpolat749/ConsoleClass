d57d7a71d77167bb1b71f9e47909f4bd6072ca25
Emin Polat <eminpolat749@gmail.com>
2023-08-15 12:50:06 +0300

    first commit

---
+++ src/com/eminpolat/util/console/test/ReadDoubleTest.java
@@ -0,0 +1,17 @@
+package com.eminpolat.util.console.test;
+
+import com.eminpolat.util.console.Console;
+
+public class ReadDoubleTest {
+    public static void run()
+    {
+        double a = Console.readDouble("Birinci sayıyı giriniz:", "Hatalı sayı girdiniz!...");
+        double b = Console.readDouble("İkinci sayıyı giriniz:", "Hatalı sayı girdiniz!...");
+
+        Console.writeLine("%f + %f = %f", a, b, a + b);
+    }
+    public static void main(String[] args)
+    {
+        run();
+    }
+}
---
+++ src/com/eminpolat/util/console/test/ReadIntTest.java
@@ -0,0 +1,17 @@
+package com.eminpolat.util.console.test;
+
+import com.eminpolat.util.console.Console;
+
+public class ReadIntTest {
+    public static void run()
+    {
+        int a = Console.readInt("Birinci sayıyı giriniz:", "Hatalı sayı girdiniz!...");
+        int b = Console.readInt("İkinci sayıyı giriniz:", "Hatalı sayı girdiniz!...");
+
+        Console.writeLine("%d + %d = %d", a, b, a + b);
+    }
+    public static void main(String[] args)
+    {
+        run();
+    }
+}
